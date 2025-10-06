d4cbac65e2b23ddcd15b8d98cd81d84067d0d981
Keiner Arismendy <keinerf588@gmail.com>
2025-10-06 17:48:40 -0500

    Creacion del ejercicio 4 que solicita resolver una expresion matematica misteriosa y detallando como se resuelve, usando una estructura algoritmica en JS

---
+++ Scripts/ej4.js
@@ -0,0 +1,14 @@
+/*
+La operación misteriosa
+Se tiene la expresión matemática siguiente: (20 + 10) / 5 + 3 * 2 - 4
+*/
+
+// --- PROCESO ---
+// JavaScript respeta el orden de las operaciones (paréntesis, multiplicación/división, suma/resta).
+// Por lo tanto, podemos escribir la expresión directamente en una sola línea.
+let resultado = (20+10)/5+3*2-4*(20 + 10) / 5 + 3 * 2 - 4*(20+10)/5+3*2-4;
+
+
+// --- SALIDA ---
+// Mostramos el resultado final.
+alert("El resultado de la operación es: " + resultado);
